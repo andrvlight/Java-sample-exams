1c029a28c64a0adabfbe44f49a7f31dbda8281e5
Andrii Likhtarov <andrvlight@gmail.com>
2025-05-18 16:49:46 +0200

    done rental part1

--- rental/CarRental.java
+++ rental/CarRental.java
@@ -1,5 +1,48 @@
 package rental;
 
-public class Car Rental {
-    
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CarRental {
+
+    private ArrayList<Car> cars;
+
+    public CarRental (String filename) {
+        try (Scanner scanner = new Scanner (new File (filename))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                try {
+                    String[] cars = line.split(":");
+                    if (cars.length != 2)
+                        continue;
+                    String brand = cars[0];
+
+                    String[] platePrice = cars[1].split(",");
+                    if (platePrice.length != 2)
+                        continue;
+                    String licensePlate = platePrice[0];
+                    double price = platePrice[1];
+
+                    Car car = Car.make(brand, licensePlate, price);
+                    if (car != null)
+                        this.cars.add(Car);
+                } catch (Exception e) {
+
+                }
+            }
+        } catch (FileNotFoundException e) {
+
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Car car : cars)
+            sb.append(car.toString()).append("\n");
+
+        return sb.toString();
+    }
 }
